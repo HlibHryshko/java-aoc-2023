6163c09440addaa9438616ca5e14ff5e60e5f623
Hlib Hryshko <glebg27@gmail.com>
2024-11-26 17:47:38 +0100

    util class for reading from file is added

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Util {
+    public static List<String> readFile(String fileName) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
+    }
+}
